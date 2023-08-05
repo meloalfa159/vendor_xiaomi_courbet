305ee623b537866b602013fd18caae1be806bbbb
basamaryan <basam.aryan@gmail.com>
2023-08-05 17:54:12 +0200

    courbet: Provide partition dependent module name if necessary
    Change-Id: I46404c49c9624ab0ba725aadaa595545de53615e

--- courbet-vendor.mk
+++ courbet-vendor.mk
@@ -326,7 +326,7 @@ PRODUCT_PACKAGES += \
     libmialgo_sd \
     libmialgoengine \
     libmibokeh_712 \
-    libmpbase \
+    libmpbase_vendor \
     libmulticam_image_optical_zoom \
     libmulticam_video_optical_zoom \
     libnotifyaudiohal \
